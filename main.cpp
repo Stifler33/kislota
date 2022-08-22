647a65f608f242fd03f7d7355f24be84ab6a51d5
Stifler33 <bagi33.ru@gmail.com>
2022-08-22 21:58:51 +0300

    working

--- main.cpp
+++ main.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
-#include "header.h"
 #include <vector>
-#include <sstream>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
+void result(int value)
+{
+    ofstream file("output.txt");
+    file << value;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     ifstream file("..\\input.txt");
-    if (file.is_open()){
-        cout << "file open\n";
-    }else{
-        cerr << "file not open\n";
-        return -1;
-    }
     int tank;
     vector<int> value;
     int max = 0;
-    int min = INT_MAX;
+    int min = INT8_MAX;
     while(!file.eof())
     {
         if (file.tellg() == 0) {
@@ -31,6 +28,40 @@ int main() {
         }
 
     }
-
+    if (value.size() != tank) {
+        result(-1);
+        return 0;
+    }
+    if (max == min) {
+        result(0);
+        return 0;
+    }
+    if (*value.begin() == max) {
+        result(-1);
+        return -1;
+    }
+    auto posMax = find(value.begin(), value.end(), max);
+    for (auto i = value.begin(); i != value.end(); i++)
+    {
+        if (*i != max && *i != min) {
+            result(-1);
+            return -1;
+        }
+        if (i >= value.begin() && i < posMax)
+        {
+            if (*i != min) {
+                result(-1);
+                return -1;
+            }
+        }
+        if (i >= posMax && i < value.end())
+        {
+            if (*i != max) {
+                result(-1);
+                return -1;
+            }
+        }
+    }
+    result(max - min);
     return 0;
 }
